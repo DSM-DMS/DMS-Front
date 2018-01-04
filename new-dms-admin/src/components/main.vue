7682161eb0d9f8c2d04a05a8b163106f11911a22
khg0712 <k7120792@gmail.com>
2018-01-04 10:27:51 +0900

    미세한 조정및 서버와의 연결

--- new-dms-admin/src/components/main.vue
+++ new-dms-admin/src/components/main.vue
@@ -36,18 +36,18 @@
         <div id="management-title">
             <h3 id="title">Management</h3>
         </div>
-         <div id="management-contents">
+         <div id="management-contents" @click="changeView('manage')">
             <div class="management-bg">
                 <img class="icon" src="../assets/icon/ic_notice.png">
                 <h3 class="conetnets-title">공지사항</h3>
                 <p class="sentence">공지사항을 작성,관리합니다.</p>
             </div>
-            <div class="management-bg">
+            <div class="management-bg" @click="changeView('survey')">
                 <img class="icon" src="../assets/icon/ic_rule.png">
                 <h3 class="conetnets-title">기숙사규칙</h3>
                 <p class="sentence">기숙사규칙을 작성,관리합니다.</p>
             </div>
-            <div class="management-bg">
+            <div class="management-bg" @click="changeView('survey')">
                 <img class="icon" src="../assets/icon/ic_question.png">
                 <h3 class="conetnets-title" >자주하는 질문</h3>
                 <p class="sentence">자주하는 질문을 작성,관리합니다.</p>
