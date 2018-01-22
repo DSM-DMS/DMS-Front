6fc2bd45252e7c1716491af25d670c4296d9ca31
khg0712 <k7120792@gmail.com>
2018-01-22 17:36:14 +0900

    all-manage-components are done
    they work well

--- new-dms-admin/src/components/manage/child/manage-writing.vue
+++ new-dms-admin/src/components/manage/child/manage-writing.vue
@@ -1,23 +1,47 @@
 <template>
-  <div>
-    <input id="title-input" placeholder="제목"/>
+  <div id="container">
+    <input id="title-input" placeholder="제목" v-model="title"/>
     <div id="app">
-      <ckeditor v-model="content" :config="config" @blur="onBlur($event)" @focus="onFocus($event)"/>
+      <ckeditor v-model="content" 
+      :config="config" 
+      @blur="onBlur($event)" 
+      @focus="onFocus($event)"/>
     </div>
-    <button id="submit">작성완료</button>
+    <button id="submit" @click="finished">작성 완료</button>
   </div>
 </template>
 
 <script>
 import Ckeditor from 'vue-ckeditor2'
 
-// const qs = require('query-string')
-
 export default {
+  watch: {
+    modifyCase: function (newVal) {
+      if (newVal) {
+        this.modifyId = this.modifyPostId
+        this.content = this.modifyPostContent
+        this.title = this.modifyPostTitle
+      } else {
+        this.modifyId = ''
+        this.content = ''
+        this.title = ''
+      }
+    }
+  },
+  created () {
+    if (this.modifyCase) {
+      this.modifyId = this.modifyPostId
+      this.content = this.modifyPostContent
+      this.title = this.modifyPostTitle
+    }
+  },
   components: { Ckeditor },
+  props: ['modifyPostId', 'modifyPostTitle', 'modifyPostContent', 'modifyCase'],
   data: function () {
     return {
+      title: '',
       content: '',
+      modifyId: '',
       config: {
         toolbar: [
           { name: 'forms', items: [ 'Checkbox', 'Radio', 'Button' ] },
@@ -31,11 +55,68 @@ export default {
           { name: 'tools', items: [ 'Maximize', 'ShowBlocks' ] },
           { name: 'about', items: [ 'About' ] }
         ],
-        height: 200
+        height: 300
       }
     }
   },
   methods: {
+    onBlur (editor) {
+      console.log(editor)
+    },
+    onFocus (editor) {
+      console.log(editor)
+    },
+    submitPosts: function () {
+      console.log('submit')
+      let formData = new FormData()
+      formData.append('title', this.title)
+      formData.append('content', this.content)
+      this.$axios.post('/admin/notice', formData,
+        {
+          headers: {
+            Authorization: 'JWT ' + this.$getCookie('JWT')
+          }
+        })
+      .then(response => {
+        if (response === 201) {
+          console.log('공지사항 업로드 성공')
+          alert('공지사항 업로드 성공')
+        }
+      })
+      .catch(e => {
+        console.log('Error', e)
+      })
+    },
+    patchPosts: function () {
+      console.log('patch')
+      let formData = new FormData()
+      formData.append('id', this.modifyId)
+      formData.append('title', this.title)
+      formData.append('content', this.content)
+      this.$axios.patch('/admin/notice', formData,
+        {
+          headers: {
+            Authorization: 'JWT ' + this.$getCookie('JWT')
+          }
+        })
+      .then(response => {
+        if (response === 200) {
+          console.log('공지사항 수정 성공')
+        }
+      })
+      .catch(e => {
+        console.log('ERROR ==>', e)
+      })
+    },
+    finished: function () {
+      if (this.modifyCase) {
+        console.log('수정')
+        this.patchPosts()
+      } else {
+        console.log('제출')
+        this.submitPosts()
+      }
+    }
   }
 }
 </script>
@@ -56,12 +137,13 @@ export default {
 }
 
 #submit {
-  margin-top: 3vh;
-  height: 40px;
-  width: 100px;
+  height: 12%;
+  width: 13%;
   border: none;
   background-color: #675094;
   color: #fff;
-  border-radius: 20px;
+  border-radius: 50px;
+  font-size: 20px;
+  margin: auto;
 }
 </style>
