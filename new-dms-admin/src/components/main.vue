2f23c6271245f410848f07bdde79b28c025a73f7
jieunjieun <kyn07077@gmail.com>
2018-02-08 19:43:06 +0900

    admin 메인 페이지 리팩토링

--- new-dms-admin/src/components/main.vue
+++ new-dms-admin/src/components/main.vue
@@ -1,73 +1,45 @@
 <template>
 <div id="main-page">
-    <div id="main-bg"></div>
-    <div class="top">
-        <img id="logo"  src="../assets/logo/logo.png">
-        <div id="admin-account">
-            <div id="account-contents" @click="showAdminDelete()">
-                <img id="admin-delete" src="../assets/icon/ic_create_account.png">
-                <p id="account-title">관리자 계정 삭제</p> 
-            </div> 
-            <div id="account-contents" @click="showAdminAccount()">
-                <img id="admin-create" src="../assets/icon/ic_return_account.png">
-                <p id="account-title">관리자 계정 생성</p>
-            </div> 
+    <div class = "top">
+        <div class = "logo"></div>
+        <div id = "admin-account">
+            <div class = "admin-create" @click="showAdminAccount()"><div class = "create-icon"></div>관리자 계정 생성</div>
+            <div class = "admin-delete" @click="showAdminDelete()"><div class = "delete-icon"></div>계정 초기화</div>
         </div>
     </div>
     <div id="contents">
-        <div id="download-title">
-            <h3 id="title">Download</h3>
-        </div>
-        <div id="download-contents">
-            <div class="download-bg" @click="stay">
-                <img class="icon" src="../assets/icon/ic_stay.png">
-                <h3 class="conetnets-title">잔류신청</h3>
-                <p class="sentence">잔류 신청 결과를 다운로드합니다.</p>
+        <div class = "download-contents">
+            <div class = "select-icon">
+                <div class = "select-inner">
+                    <div class = "download-logo"></div>
+                    Download
+                </div>
             </div>
-            <div class="download-bg" @click="goingout">
-                <img class="icon" src="../assets/icon/ic_goingout.png">
-                <h3 class="conetnets-title">외출신청</h3>
-                <p class="sentence">외출 신청 결과를 다운로드합니다.</p>
-            </div>
-            <div class="download-bg" @click="facility">
-                <img class="icon" src="../assets/icon/ic_broken.png">
-                <h3 class="conetnets-title">시설고장</h3>
-                <p class="sentence">시설 고장 목록을 다운로드합니다.</p>
-            </div>
-            <div class="download-bg" @click="extension_11">
-                <img class="icon" src="../assets/icon/ic_eleven.png">
-                <h3 class="conetnets-title">11시연장</h3>
-                <p class="sentence">연장 신청 목록을 다운로드합니다.</p>
-            </div>
-            <div class="download-bg" @click="extension_12">
-                <img class="icon" src="../assets/icon/ic_twelve.png">
-                <h3 class="conetnets-title">12시연장</h3>
-                <p class="sentence">연장 신청 목록을 다운로드합니다.</p>
+            <div class = "download-body">
+                <div class = "select-box" v-for="item in downloadList" :key = "item.id" >
+                    <div class = "select-content" @click='selectMode(item.title)'>
+                        <div class = "select-box-logo"><img :src="item.logo"/></div>
+                        <div class = "select-title">{{item.title}}</div>
+                        <div class = "select-description">{{item.description}}</div>
+                    </div>
+                </div>
             </div>
         </div>
-        <div id="management-title">
-            <h3 id="title">Management</h3>
-        </div>
-         <div id="management-contents">
-            <div class="management-bg" @click="changeView('notice')">
-                <img class="icon" src="../assets/icon/ic_notice.png">
-                <h3 class="conetnets-title">공지사항</h3>
-                <p class="sentence">공지사항을 작성,관리합니다.</p>
+        <div class = "Management-contents">
+            <div class = "select-icon">
+                <div class = "select-inner">
+                    <div class = "management-logo"></div>
+                    Management
+                </div>
             </div>
-            <div class="management-bg" @click="changeView('rule')">
-                <img class="icon" src="../assets/icon/ic_rule.png">
-                <h3 class="conetnets-title">기숙사규칙</h3>
-                <p class="sentence">기숙사규칙을 작성,관리합니다.</p>
-            </div>
-            <div class="management-bg" @click="changeView('faq')">
-                <img class="icon" src="../assets/icon/ic_question.png">
-                <h3 class="conetnets-title" >자주하는 질문</h3>
-                <p class="sentence">자주하는 질문을 작성,관리합니다.</p>
-            </div>
-            <div class="management-bg" @click="changeView('survey')">
-                <img class="icon" src="../assets/icon/ic_poll.png">
-                <h3 class="conetnets-title">설문조사</h3>
-                <p class="sentence">설문조사를 작성,관리합니다.</p>
+            <div class = "management-body">
+                <div class = "select-box" v-for="item in manageList" :key = "item.id" >
+                    <div class = "select-content" @click='selectMode(item.title)'>
+                        <div class = "select-box-logo"><img :src="item.logo"/></div>
+                        <div class = "select-title">{{item.title}}</div>
+                        <div class = "select-description">{{item.description}}</div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
@@ -90,7 +62,61 @@ export default {
   data: function () {
     return {
       isAdminAccount: false,
-      isAdminDelete: false
+      isAdminDelete: false,
+      downloadList: [
+        {
+          logo: require('../assets/icon/ic_stay.png'),
+          title: '잔류신청',
+          description: '잔류신청 결과를 다운로드 합니다.',
+          event: 'hello'
+        },
+        {
+          logo: require('../assets/icon/ic_goingout.png'),
+          title: '외출신청',
+          description: '외출 신청 결과를 다운로드 합니다.'
+        },
+        {
+          logo: require('../assets/icon/ic_broken.png'),
+          title: '시설고장',
+          description: '시설 고장 목록을 다운로드 합니다.'
+        },
+        {
+          logo: require('../assets/icon/ic_eleven.png'),
+          title: '11시연장',
+          description: '연장 신청 목록을 다운로드 합니다.'
+        },
+        {
+          logo: require('../assets/icon/ic_twelve.png'),
+          title: '12시연장',
+          description: '연장 신청 목록을 다운로드 합니다.'
+        }
+      ],
+      manageList: [
+        {
+          logo: require('../assets/icon/ic_notice.png'),
+          title: '공지사항',
+          description: '공지사항을 작성, 관리합니다.',
+          event: 'helo'
+        },
+        {
+          logo: require('../assets/icon/ic_rule.png'),
+          title: '기숙사규칙',
+          description: '기숙사 규칙을 작성, 관리합니다. ',
+          event: 'helo'
+        },
+        {
+          logo: require('../assets/icon/ic_question.png'),
+          title: '자주하는 질문',
+          description: '자주하는 질문을 작성, 관리합니다.',
+          event: 'helo'
+        },
+        {
+          logo: require('../assets/icon/ic_poll.png'),
+          title: '설문조사',
+          description: '설문조사를 작성, 관리합니다.',
+          event: 'helo'
+        }
+      ]
     }
   },
   methods: {
@@ -102,6 +128,35 @@ export default {
     },
     showAdminDelete: function (event) {
       this.isAdminDelete = !this.isAdminDelete
+    },
+    selectMode: function (mode) {
+      if (mode === '잔류신청') {
+        this.stay()
+      }
+      if (mode === '외출신청') {
+        this.goingout()
+      }
+      if (mode === '시설고장') {
+        this.facility()
+      }
+      if (mode === '11시연장') {
+        this.extension_11()
+      }
+      if (mode === '12시연장') {
+        this.extension_12()
+      }
+      if (mode === '공지사항') {
+        this.changeView('notice')
+      }
+      if (mode === '기숙사규칙') {
+        this.changeView('rule')
+      }
+      if (mode === '자주하는 질문') {
+        this.changeView('faq')
+      }
+      if (mode === '설문조사') {
+        this.changeView('survey')
+      }
     },
     stay: function (event) {
       this.$axios({
@@ -183,113 +238,222 @@ export default {
       box-sizing: border-box;
       font-family: 'Noto Sans KR', sans-serif;
   }
+
+  /* 전체 페이지 */
   
   #main-page {
       width: 100vw;
       height: 100vh;
+      position: relative;
+      overflow: auto;
+      background-image: url('../assets/background/bg_notice.png');
+      background-size: cover;
+      background-repeat: no-repeat;
   }
 
-  #main-bg {
-      width: 100vw;
-      height: 100vh;
-      background-image: url("../assets/background/bg_notice.png");
-      position: absolute;
-  }
+
+/* 로고 + 관리자 계정 박스 (탑바) */
 
   .top {
       width: 100%;
-      height: 100px;
-      position: fixed;
+      height: 150px;
+      display: flex;
+      align-items: center;
   }
+
+/* 로고 */
+
+  .logo {
+    background-image: url('../assets/logo/logo.png');
+    position: absolute;
+    background-size: 80% 80%;
+    width: 148px;
+    height: 50px;
+    background-repeat: no-repeat;
+    margin-left: 50px;
+  }
+
+
+/* 관리자 계정 박스  */
 
   #admin-account {
-      width: 500px;
-      height: 100px;
-      position: relative;
-      top: 25px;
-      right: 30px;
-      float: right;
+    position: absolute;
+    right: 30px;
+    display: flex;
   }
 
-  #account-title {
-      display: inline-block;
-      font-weight: 800;
-  }
+  /* 관리자 계정 생성  */
 
-  #admin-create, #admin-delete {
-      vertical-align: middle;
-      margin-right: 10px;
-  }
-  
-  #account-contents {
-      display: inline-block;
-      margin-left: 50px;
+  .admin-create {
+      width:200px;
+      height: 50px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      font-weight: bold;
+      font-size: 18px;
       cursor: pointer;
   }
 
-  img#logo {
+  .admin-create > .create-icon {
+      background-image: url('../assets/icon/ic_return_account.png');
+      background-size: 100% auto;
+      width: 25px;
+      height: 25px;
+      margin-right: 15px;
+      background-repeat: no-repeat;
+  }
+
+  /* 계정 초기화  */
+
+  .admin-delete {
+      width:200px;
+      height: 50px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      font-weight: bold;
+      font-size: 18px;
+      cursor: pointer;
+  }
+  
+
+  .admin-delete > .delete-icon {
+      background-image: url('../assets/icon/ic_create_account.png');
+      background-size: 100% auto; 
+      width: 25px; 
+      height: 25px;
+      margin-right: 15px;
+      background-repeat: no-repeat;
+  }
+
+
+/* 다운로드 */
+
+  .download-contents {
+        margin-bottom: 50px;
+    }
+
+  .select-icon {
+      width: 250px;
+      height: 60px;
       position: relative;
-      float: left;
-      top: 30px;
-      left: 30px;
+      left: 0;
+      z-index: 3;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      margin: 30px;
+  }
+
+  .select-icon::after {
+      width: 250px;
+      border-radius: 70px;
+      height: 60px;
+      content: "";
+      position: absolute;
+      background-color: white;
+      opacity: 0.5;
+      z-index: 1;
+  }
+
+  .select-inner {
+      position: absolute;
+      z-index: 2;
+      color: #675094;
+      font-weight: bold;
+      font-size: 27px;
+      letter-spacing: -1px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+  }
+
+  .download-logo {
+      width: 40px;
+      height: 40px;
+      margin-right: 10px;
+      background-image: url('../assets/icon/ic_download2.png');
+      background-size: cover;
+      background-repeat: no-repeat;
+  }
+
+
+  .download-body {
+      width: 1300px;
+      height: 235px;
+      overflow-x: scroll;
+      overflow-y: hidden;
+      align-items: center;
+      white-space: nowrap;
+      position: relative;
+      display: inline-block;
+      /* display: inline; */
+  }
+
+ /* 박스 */
+  .select-box {
+      width: 300px;
+      height: 200px;
+      border-radius: 8px;
+      background-color: white;
+      margin: 10px;
+      white-space: nowrap;
+      display: inline-block;
+      position: relative;
+  }
+
+  .select-content {
+      position: absolute;
+      width: 100%;
+      height: 100%;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      flex-direction: column;
+      cursor: pointer;
+  }
+
+  .select-box-logo > img {
+      width: 70px;
+      height: 70px;
+  }
+
+  .select-title {
+      font-size: 28px;
+      font-weight: bold;
+  }
+
+  .select-description {
+      font-size: 14px;
+      font-weight: bold;
+  }
+
+  /* management */
+
+  .management-logo {
+      width: 40px;
+      height: 40px;
+      margin-right: 10px;
+      background-image: url('../assets/icon/ic_setting2.png');
+      background-size: cover;
+      background-repeat: no-repeat;
+  }
+
+  .management-body {
+      width: 1300px;
+      height: 235px;
+      overflow: hidden;
+      align-items: center;
+      white-space: nowrap;
+      display: inline-block;
+
   }
 
   #contents {
       width: 100%;
-      height: 88vh;
+      height: calc(100% -150px);
       position: absolute;
-      margin-top: 100px;
   }
   
-  #download-title {
-      width: 220px;
-      height: 50px;
-      border-radius: 100px;
-      background-color: rgba(202,182,177, 0.8);
-      margin-top: 70px;
-      margin-left: 100px;
-  }
-  #management-title {
-      width: 220px;
-      height: 50px;
-      border-radius: 100px;
-      background-color: rgba(202,182,177, 0.8);
-      margin-top: 80px;
-      margin-left: 100px;
-  }
-
-  h3#title {
-      font-size: 20px;
-      padding-top: 15px;
-      color: #675094;
-  }
-
-  h3.conetnets-title {
-      font-size: 30px;
-      color: #675094;
-      margin-top: 10px;
-    }
-  
-  .download-bg, .management-bg {
-      width: 17vw;
-      height: 23vh;
-      background-color: white;
-      margin-left: 30px;
-      border-radius: 10px;
-      display: inline-block;
-      margin-top: 30px;
-      cursor: pointer;
-  }
-
-  img.icon {
-      margin-top: 20px;
-  }
-
-  p.sentence {
-      font-weight: 600;
-      font-size: 12px;
-      margin-top: -15px;
-  }
-
-
 </style>
