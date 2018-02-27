3ed882663c09d2cfcffa3155d835a738168e53c9
seoyunho <yunho101775@gmail.com>
2018-02-27 15:42:27 +0900

    add footer name

--- dms/src/components/FooterSection/DeveloperIntro.vue
+++ dms/src/components/FooterSection/DeveloperIntro.vue
@@ -13,11 +13,11 @@ export default {
   data: function () {
     return {
       category: [
-        {title: 'App', members: ['이병찬', '윤정현', '이성현']},
-        {title: 'Server', members: ['조민규', '인상민']},
-        {title: 'Web Front', members: ['서윤호', '김 건', '김형규', '오인서', '윤효상']},
-        {title: 'Desktop', members: ['김동현', '이종현', '류근찬']},
-        {title: 'Design', members: ['김동규']}
+        {title: 'App', members: ['조성빈', '이병찬', '윤정현', '이성현']},
+        {title: 'Server', members: ['김성래', '인상민', '조민규']},
+        {title: 'Web Front', members: ['김지수', '김 건', '서윤호', '김형규', '오인서', '윤효상']},
+        {title: 'Desktop', members: ['김경식', '정원태', '김동현', '이종현', '류근찬']},
+        {title: 'Design', members: ['윤여환', '김동규']}
       ]
     }
   }
