f5c7a340815c2f64899493a0f244b964f3e50bbb
unknown <geon0250@gmail.com>
2018-03-05 14:41:08 +0900

    모바일 접속시 리다이렉션

--- dms/src/App.vue
+++ dms/src/App.vue
@@ -8,6 +8,11 @@
 export default {
   name: 'app',
   beforeCreate: function () {
+    if (navigator.userAgent.match(/iPhone/) || navigator.userAgent.match(/iPad/)) {
+      this.$router.push('https://itunes.apple.com/us/app/dms-for-dsm/id1328234395?l=ko&ls=1&mt=8')
+    } else if (navigator.userAgent.match(/Android/)) {
+      this.$router.push('https://play.google.com/store/apps/details?id=teamdms.dms_kotlin')
+    }
     this.$store.dispatch('getData')
   },
   beforeUpdate: function () {
